45b313e3824ba0662342b1d7e7bb830a99816082
A7337li <alizahra7337@gmail.com>
2022-11-06 14:12:02 +0330

    Add javascript code for the mobile part of menu bar

--- Main-bar.js
+++ Main-bar.js
@@ -1,6 +1,20 @@
 const $ = document;
 const phone = $.querySelector("div.phone-0");
-let pool = true;
+let bool = true;
 
 
-
+phone.addEventListener("click",function(){
+    if (bool == true) {
+        $.querySelector("p.mobile").style.color = "red";
+        $.querySelector("div.subset-mobile").style.display = "inline";
+        $.querySelector("svg.angle-down-0").style.display = "none";
+        $.querySelector("svg.angle-up-0").style.display = "inline";
+        bool = false;
+    } else {
+        $.querySelector("p.mobile").style.color = "black";
+        $.querySelector("div.subset-mobile").style.display = "none";
+        $.querySelector("svg.angle-up-0").style.display = "none";
+        $.querySelector("svg.angle-down-0").style.display = "inline";
+        bool = true;
+    }
+});
