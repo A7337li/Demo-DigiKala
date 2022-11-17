bb7cb1c91219b7c93ffd3c7fe30580894b9977ea
A7337li <alizahra7337@gmail.com>
2022-11-17 21:29:00 +0330

    add new javascript code for open & closed of 'brandhaye goshi'

--- Main-bar.js
+++ Main-bar.js
@@ -1,20 +1,38 @@
 const $ = document;
-const phone = $.querySelector("div.phone-0");
-let bool = true;
+const phone = $.querySelectorAll("div.angle")[0];
+const brand_goshi = $.querySelectorAll("div.angle")[1];
+let bool_0 = true;
+let bool_1 = true;
 
 
 phone.addEventListener("click", function () {
-    if (bool == true) {
-        $.querySelector("p.mobile").style.color = "red";
-        $.querySelector("div.subset-mobile").style.display = "inline";
+    if (bool_0 == true) {
+        $.querySelector("p.redcolor0").style.color = "red";
+        $.querySelector("div.display0").style.display = "inline";
         $.querySelector("svg.angle-down-0").style.display = "none";
         $.querySelector("svg.angle-up-0").style.display = "inline";
-        bool = false;
+        bool_0 = false;
     } else {
-        $.querySelector("p.mobile").style.color = "black";
-        $.querySelector("div.subset-mobile").style.display = "none";
+        $.querySelector("p.redcolor0").style.color = "black";
+        $.querySelector("div.display0").style.display = "none";
         $.querySelector("svg.angle-up-0").style.display = "none";
         $.querySelector("svg.angle-down-0").style.display = "inline";
-        bool = true;
+        bool_0 = true;
+    }
+});
+
+brand_goshi.addEventListener("click" , function(){
+    if (bool_1 == true) {
+        $.querySelector("p.redcolor1").style.color = "red";
+        $.querySelector("div.display1").style.display = "inline";
+        $.querySelector("svg.angle-down-1").style.display = "none";
+        $.querySelector("svg.angle-up-1").style.display = "inline";
+        bool_1 = false;
+    } else {
+        $.querySelector("p.redcolor1").style.color = "black";
+        $.querySelector("div.display1").style.display = "none";
+        $.querySelector("svg.angle-up-1").style.display = "none";
+        $.querySelector("svg.angle-down-1").style.display = "inline";
+        bool_1 = true; 
     }
 });
