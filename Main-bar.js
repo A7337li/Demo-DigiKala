07cc98d749637949b41f71a0b492a9900fc6bb61
A7337li <alizahra7337@gmail.com>
2022-11-29 14:01:01 +0330

    changed javascript

--- Main-bar.js
+++ Main-bar.js
@@ -1,38 +1,19 @@
 const $ = document;
-const phone = $.querySelectorAll("div.angle")[0];
-const brand_goshi = $.querySelectorAll("div.angle")[1];
-let bool_0 = true;
-let bool_1 = true;
+let bool= true;
 
 
-phone.addEventListener("click", function () {
-    if (bool_0 == true) {
+function myfunction() {
+    if (bool == true) {
         $.querySelector("p.redcolor0").style.color = "red";
         $.querySelector("div.display0").style.display = "inline";
         $.querySelector("svg.angle-down-0").style.display = "none";
         $.querySelector("svg.angle-up-0").style.display = "inline";
-        bool_0 = false;
+        bool = false;
     } else {
         $.querySelector("p.redcolor0").style.color = "black";
         $.querySelector("div.display0").style.display = "none";
         $.querySelector("svg.angle-up-0").style.display = "none";
         $.querySelector("svg.angle-down-0").style.display = "inline";
-        bool_0 = true;
+        bool = true;
     }
-});
-
-brand_goshi.addEventListener("click" , function(){
-    if (bool_1 == true) {
-        $.querySelector("p.redcolor1").style.color = "red";
-        $.querySelector("div.display1").style.display = "inline";
-        $.querySelector("svg.angle-down-1").style.display = "none";
-        $.querySelector("svg.angle-up-1").style.display = "inline";
-        bool_1 = false;
-    } else {
-        $.querySelector("p.redcolor1").style.color = "black";
-        $.querySelector("div.display1").style.display = "none";
-        $.querySelector("svg.angle-up-1").style.display = "none";
-        $.querySelector("svg.angle-down-1").style.display = "inline";
-        bool_1 = true; 
-    }
-});
+}
