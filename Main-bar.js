81481772f8aa66f2ddeb4642ebdddeb20074c2e9
A7337li <alizahra7337@gmail.com>
2023-01-14 15:49:27 +0330

    changed 'if' of myfunction

--- Main-bar.js
+++ Main-bar.js
@@ -1,19 +1,17 @@
 const $ = document;
-let bool= true;
 
-
-function myfunction() {
-    if (bool == true) {
-        $.querySelector("p.redcolor0").style.color = "red";
-        $.querySelector("div.display0").style.display = "inline";
-        $.querySelector("svg.angle-down-0").style.display = "none";
-        $.querySelector("svg.angle-up-0").style.display = "inline";
+function myfunction(a, b, c, d) {
+    if ($.querySelector(c).style.display == "inline") {
+        $.querySelector(a).style.color = "red";
+        $.querySelector(b).style.display = "inline";
+        $.querySelector(c).style.display = "none";
+        $.querySelector(d).style.display = "inline";
         bool = false;
     } else {
-        $.querySelector("p.redcolor0").style.color = "black";
-        $.querySelector("div.display0").style.display = "none";
-        $.querySelector("svg.angle-up-0").style.display = "none";
-        $.querySelector("svg.angle-down-0").style.display = "inline";
+        $.querySelector(a).style.color = "black";
+        $.querySelector(b).style.display = "none";
+        $.querySelector(c).style.display = "inline";
+        $.querySelector(d).style.display = "none";
         bool = true;
     }
 }
