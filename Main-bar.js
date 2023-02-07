c73b76119da269213336fd675d55b2e96584b73d
A7337li <alizahra7337@gmail.com>
2023-02-07 12:09:27 +0330

    fixed the problem of 'AngleDown' not opening the first time with one click

--- Main-bar.js
+++ Main-bar.js
@@ -4,16 +4,16 @@ function myfunction(colorText, subset, angleDown, angleUp) {
 
     let angleD = $.querySelector(angleDown).style.display;
     
-    if (angleD == "inline") {
-        $.querySelector(colorText).style.color = "red";
-        $.querySelector(subset).style.display = "inline";
-        $.querySelector(angleDown).style.display = "none";
-        $.querySelector(angleUp).style.display = "inline";
-    } else {
+    if (angleD == "none") {
         $.querySelector(colorText).style.color = "black";
         $.querySelector(subset).style.display = "none";
         $.querySelector(angleDown).style.display = "inline";
         $.querySelector(angleUp).style.display = "none";
+    } else {
+        $.querySelector(colorText).style.color = "red";
+        $.querySelector(subset).style.display = "inline";
+        $.querySelector(angleDown).style.display = "none";
+        $.querySelector(angleUp).style.display = "inline";
     }
 }
 
